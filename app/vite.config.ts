96fca1a10566ebf741c2936cc841dc38d5e057b5
Debbal Lotfi <debbal.lotfi.dev@gmail.com>
2026-02-08 18:15:04 +0100

    init

---
+++ app/vite.config.ts
@@ -0,0 +1,60 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  server: {
+    allowedHosts: true,
+  },
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "IOT Commander",
+        short_name: "IOT Commander",
+        description: "An app to controll IOT devices using MQTT",
+        theme_color: "#E16349",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: false,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+  base: "/iot-commander/",
+  build: {
+    outDir: "../docs",
+    emptyOutDir: true,
+    rollupOptions: {
+      output: {
+        manualChunks: (id) => {
+          if (id.includes("@heroui")) {
+            return "heroui";
+          } else if (id.includes("mqtt")) {
+            return "mqtt";
+          } else if (id.includes("lucide-react")) {
+            return "lucide-react";
+          }
+        },
+      },
+    },
+  },
+});
